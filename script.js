651e930c327988b815d4208f66db39af2b6a7b7a
Deepak <kumardeepakbr28@gmail.com>
2024-01-11 15:17:29 +0530

    dom

---
+++ script.js
@@ -0,0 +1,16 @@
+const div = document.getElementById("dom");
+
+div.style.backgroundColor ="yellow";
+div.style.width = "320px";
+div.style.height = "220px";
+div.style.margin = "20px";
+const p = document.getElementById("para");
+
+p.style.color = "#0000ff"
+p.style.fontFamily ="Times New Roman";
+p.style.fontSize ="18px";
+p.style.fontWeight ="bold";
+// p.style.marginLeft ="60px";
+// p.style.marginTop = "60px";
+p.style.padding="10px";
+
